34131219d83eb43cba6ab768f468436222899fb7
krititiawri <kriit.tiwari28@gmail.com>
2017-09-20 20:56:27 -0500

    removing nide modules

--- key.js
+++
@@ -1,9 +0,0 @@
-// console.log('this is loaded');
-
-var twitter= require("twitter")
-exports.twitterKeys = {
-Consumer_Key :'gSyK32XxazI9BD9JNgzn1hMrg',
-Consumer_Secret: 	'p0pmoCFMx0ycOweYNrHHxIHESZQoe9WN22A4ONzSqoIQH4GaAh',
-Access_Token_key:	'576179883-tLLUJ1iqoFroS04FtDOzojC0EjfRxIr7DjFwj3Zm',
-Access_Token_Secret:	'QMmigVtsRw0osgBrjGQ1mykxMJ6DAhEb4886GOeVzSNOF',
-}
